82f7cc622a7022987a631d6c950825846aeb19fa
Martin liu <liujiabin1212@outlook.com>
2019-05-29 16:29:25 +0800

    数组更新

--- utils/ArrayUtils/ArrayUtils.php
+++ utils/ArrayUtils/ArrayUtils.php
@@ -14,14 +14,73 @@ class ArrayUtils {
 
     /**
      * 去重、去空值、获取某列
-     * @param array $arr
-     * @param string $column
+     * @param array $arr        目标数组
+     * @param string $column    列字段
+     * @param string $key       赋值索引数组key
      * @return array
      */
-    public function uniqueColumn(array $arr, string $column): array {
+    public function uniqueColumn(array $arr, string $column, $key = null): array {
         if (empty($arr)){
             return [];
         }
-        return array_unique(array_filter(array_column($arr, $column)));
+
+        return array_unique(array_filter(array_column($arr, $column, $key)));
+    }
+
+    /**
+     * 获取某列后拼装成字符串
+     * @param array $arr        目标数组
+     * @param string $column    列字段
+     * @param string $separator 字符串分隔符
+     * @return string
+     */
+    public function uniqueColumnString(array $arr, string $column, string $separator = ','): string {
+        if (empty($arr)) {
+            return '';
+        }
+
+        return implode($separator, $this->uniqueColumn($arr, $column, null));
+    }
+
+    /**
+     * 对数组根据某一字段进行排序
+     * @param $array        目标数组
+     * @param $on           排序字段
+     * @param string $order ASC正序|DESC倒序
+     * @return array
+     */
+    public function arraySort($array, $on, $order = 'ASC'): array {
+        $new_array = array();
+        $sortable_array = array();
+
+        if (count($array) > 0) {
+            foreach ($array as $k => $v) {
+                if (is_array($v)) {
+                    foreach ($v as $k2 => $v2) {
+                        if ($k2 == $on) {
+                            $sortable_array[$k] = $v2;
+                            continue;
+                        }
+                    }
+                } else {
+                    $sortable_array[$k] = $v;
+                }
+            }
+
+            switch ($order) {
+                case 'ASC':
+                    asort($sortable_array);
+                    break;
+                case 'DESC':
+                    arsort($sortable_array);
+                    break;
+            }
+
+            foreach ($sortable_array as $k => $v) {
+                $new_array[] = $array[$k];
+            }
+        }
+
+        return $new_array;
     }
 }
--- utils/ArrayUtils/README.md
+++ utils/ArrayUtils/README.md
@@ -5,5 +5,7 @@
 目录:
 
 * `uniqueColumn` array_column再封装，去重去null
+* `uniqueColumnString` array_column再封装，去重去null，后封装成字符串
+* `arraySort` asort|arsort再封装，数组根据某字段进行正序|倒序排列
 
 一些日常使用自用封装，如果喜欢请点个star哦
